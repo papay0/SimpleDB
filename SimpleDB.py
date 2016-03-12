445e8f88898dcdc93d8f3b16eda9f8611ee36bba
Arthur Papailhau <papailhau@gmail.com>
2016-03-12 01:32:07 +0100

    Change NUMEQUALTO so that there is a better complexity

--- SimpleDB.py
+++ SimpleDB.py
@@ -7,6 +7,8 @@ class SimpleDB:
         self.stack_history = []
         # Current database
         self.database = dict()
+        # Store the number of value
+        self.count_value = dict()
 
     def get(self, name):
         # if key exists: print value, otherwise: print NULL
@@ -14,6 +16,18 @@ class SimpleDB:
             print(self.database[name])
         else:
             print("NULL")
+
+    def get_value_db(self, name):
+        if name in self.database:
+            return self.database[name]
+        else:
+            return None
+
+    def get_value_count(self, value):
+        if value in self.count_value:
+            return self.count_value[value]
+        else:
+            return -1
 
     def set(self, name, value):
         if len(self.stack_history) > 0:
@@ -24,6 +38,13 @@ class SimpleDB:
             if name not in self.database:
                 self.stack_history[0][name] = None
         # Write into the database the name and the value
+        value_in_database = self.get_value_db(name)
+        value_in_count = self.get_value_count(value)
+        if value != value_in_database or value_in_count == -1:
+            if value != value_in_database:
+                self.decremente_count_value(value_in_database)
+            if value is not None:
+                self.incremente_count_value(value)
         self.write(name, value)
 
 
@@ -32,17 +53,26 @@ class SimpleDB:
         sys.exit()
 
     def unset(self, name):
+        value = self.get_value_db(name)
+        if value is not None:
+            self.decremente_count_value(value)
         # set None to the database for the key name, and move the old value to the stack_history
         if name in self.database:
             self.set(name, None)
 
     def num_equal_to(self, value):
-        # print the number of value equal to value in the database, but not yet O(log(N))
-        resu = 0
-        for key, value_db in self.database.items():
-            if value == value_db:
-                resu += 1
-        print(resu)
+        if value in self.count_value:
+            print(self.count_value[value])
+        else:
+            print("0")
+        # The previous solution was in O(N), so I added a structure count_value
+        # print the number of value equal to value in the database
+        #resu = 0
+        #for key, value_db in self.database.items():
+        #    if value == value_db:
+        #        resu += 1
+        #print(resu)
+
 
 
     def commit(self):
@@ -60,7 +90,7 @@ class SimpleDB:
         # Move the value of the Stack (old) to the Database
         if len(self.stack_history) > 0:
             for key, value in self.stack_history[0].items():
-                self.write(key, value)
+                self.set(key, value)
             self.stack_history.pop(0)
         else:
             print("NO TRANSACTION")
@@ -73,6 +103,16 @@ class SimpleDB:
         else:
             del self.database[name]
 
+    def incremente_count_value(self, value):
+        if value not in self.count_value:
+            self.count_value[value] = 0
+        self.count_value[value] += 1
+
+    def decremente_count_value(self, value):
+        if value in self.count_value:
+            self.count_value[value] -= 1
+            if self.count_value[value] <= 0:
+                del self.count_value[value]
 
     def command_valid(self, args):
         # Try: if user try to access a 'index out of range' argument
